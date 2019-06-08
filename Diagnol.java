7d5356416fb379b446a68d4328039a96ee6dee8e
priyasoundh <32993324+priyasoundh@users.noreply.github.com>
2019-06-08 10:59:47 +0530

    Create Diagnol.java

---
+++ Diagnol.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+public class Diagnol
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int a[][]=new int[n][n];
+		int sum=0;
+		for(int i=0;i<n;i++)
+		{
+			for(int j=0;j<n;j++)
+			{
+				a[i][j]=sc.nextInt();
+				if(i==j)
+				{
+					sum+=a[i][j];
+				}
+			}
+		}
+		System.out.println(sum);
+	}
+}
