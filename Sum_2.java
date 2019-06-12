3ab0770738f202a50e9712fd8cd4ed207660c937
priyasoundh <32993324+priyasoundh@users.noreply.github.com>
2019-06-12 14:44:08 +0530

    Update Sum_2.java

--- Sum_2.java
+++ Sum_2.java
@@ -7,11 +7,11 @@ public class Sum_2
 		Scanner sc=new Scanner(System.in);
 		int n=sc.nextInt();
 		int a[]=new int[n];
-		int count=0;
 		for(int i=0;i<n;i++)
 		{
 			a[i]=sc.nextInt();
 		}
+		int count=0;
 		for(int i=0;i<n-2;i++)
 		{
 			for(int j=i+1;j<n-1;j++)
