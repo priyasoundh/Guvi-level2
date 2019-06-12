28411de6ade6e777c74748cc948892fab0419893
priyasoundh <32993324+priyasoundh@users.noreply.github.com>
2019-06-12 14:34:44 +0530

    Create Sum_power.java

---
+++ Sum_power.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+public class Sum_power
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int a[]=new int[10];
+		int i=0;
+		while(n>0)
+		{
+			a[i]=n%10;
+			n=n/10;
+			i++;
+		}
+		n=i;
+		int j=0;
+		double sum=0;
+		for(i=n-1;i>=0;i--)
+		{
+			sum=sum+Math.pow(a[i],j);
+			j++;
+		}
+		i=(int)sum;
+		System.out.println(i);
+	}
+}
