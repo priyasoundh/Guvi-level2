43908894c1b67165187f9827ad6e6f3fc2fb46d2
priyasoundh <32993324+priyasoundh@users.noreply.github.com>
2019-06-14 11:14:19 +0530

    Create Sum_0.java

---
+++ Sum_0.java
@@ -0,0 +1,36 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+public class Sum_0
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int a[]=new int[n];
+		for(int i=0;i<n;i++)
+		{
+			a[i]=sc.nextInt();
+		}
+		int sum,b=100,c=0,d=0;
+		for(int i=0;i<n-1;i++)
+		{
+			for(int j=i+1;j<n;j++)
+			{
+				sum=a[i]+a[j];
+				if(Math.abs(sum)<b)
+				{
+					b=Math.abs(sum);
+					c=a[j];
+					d=a[i];
+					if(a[i]>a[j])
+					{
+						c=a[i];
+						d=a[j];
+					}
+				}
+			}
+		}
+		System.out.println(c+" "+d);
+	}
+}
