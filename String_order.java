4fa545992d20674791e2bafdda4be661b2312860
priyasoundh <32993324+priyasoundh@users.noreply.github.com>
2019-06-19 11:22:19 +0530

    Create String_order.java

---
+++ String_order.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+public class String_order
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		String s=sc.next();
+		String s1=sc.next();
+		System.out.println(s1+" "+s);
+	}
+}
